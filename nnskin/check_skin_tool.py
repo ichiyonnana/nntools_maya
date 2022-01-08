290451929cdcc8773bf40fa80853866b7d14002c
ichiyonnana <ichiyonnana+github@gmail.com>
2022-01-09 03:56:18 +0900

    nnskin ウェイトチェックツール実装

--- nnskin/check_skin_tool.py
+++ nnskin/check_skin_tool.py
@@ -4,7 +4,9 @@
 
 """
 import maya.cmds as cmds
+import maya.mel as mel
 import pymel.core as pm
+import pymel.core.nodetypes as nt
 
 import nnutil.core as nu
 import nnutil.decorator as deco
@@ -17,6 +19,43 @@ dialog_name = "NN_SkinChecker"
 
 default_translate_factor = 10
 default_rotate_factor = 180
+
+
+def is_weight_paint_mode():
+    return pm.currentCtx() == "artAttrSkinContext"
+
+
+def activate_weight_paint_mode():
+    pm.setToolTo("artAttrSkinContext")
+
+
+def activate_select_mode():
+    pm.setToolTo("selectSuperContext")
+
+
+def focus_object():
+    pass
+
+
+def weight_paint_mode_with_selected_joint(joint=None, meshes=[]):
+    """選択したメッシュに対して選択したジョイントがアクティブな状態でウェイトペイントモードに入る"""
+    if not joint:
+        joint = [x for x in pm.selected(flatten=True) if isinstance(x, nt.Joint)][0]
+    
+    if not meshes:
+        meshes = [x for x in pm.selected(flatten=True) if isinstance(x, nt.Transform)]
+
+    if joint and meshes:
+        print("select: ", meshes)
+        pm.select(meshes, replace=True)
+        mel.eval("ArtPaintSkinWeightsToolOptions")
+        mel.eval('artSkinInflListChanging "%s" 1' % joint.name())
+        mel.eval("artSkinInflListChanged artAttrSkinPaintCtx")
+
+
+def change_paint_target_influence(joint):
+    mel.eval('artSkinInflListChanging "%s" 1' % joint.name())
+    mel.eval("artSkinInflListChanged artAttrSkinPaintCtx")
 
 
 class TRS():
@@ -61,8 +100,10 @@ class NN_ToolWindow(object):
         ui.column_layout()
 
         ui.row_layout()
-        ui.header()
-        ui.button(label="Set Root", c=self.onSetRoot)
+        ui.header(label="Set")
+        ui.button(label="Root", c=self.onSetRoot)
+        ui.button(label="Joints", c=self.onSetJoints)
+        ui.button(label="Meshes", c=self.onSetMeshes)
         ui.end_layout()
 
         ui.row_layout()
@@ -79,9 +120,15 @@ class NN_ToolWindow(object):
 
         ui.row_layout()
         ui.header(label="")
-        ui.button(label="Prev", c=self.onPrev)
-        ui.button(label="Reset", c=self.onReset)
-        ui.button(label="Next", c=self.onNext)
+        ui.text(label="Meshes :", width=ui.width3)
+        self.text_meshes = ui.text(label="None", width=ui.width3)
+        ui.end_layout()
+
+        ui.row_layout()
+        ui.header(label="")
+        ui.button(label="Prev", c=self.onPrevNoFocus, dgc=self.onPrevFocus)
+        ui.button(label="Reset", c=self.onResetNoFocus, dgc=self.onResetFocus)
+        ui.button(label="Next", c=self.onNextNoFocus, dgc=self.onNextFocus)
         ui.end_layout()
 
         ui.separator(height=ui.height1)
@@ -130,6 +177,7 @@ class NN_ToolWindow(object):
         ui.header(label="")
         ui.button(label="Gradation", c=self.onGradation)
         ui.button(label="Animation", c=self.onAnimation)
+        ui.button(label="PaintMode", c=self.onPaintMode)
         ui.end_layout()
 
         ui.row_layout()
@@ -161,9 +209,29 @@ class NN_ToolWindow(object):
         for i, joint in enumerate(self.joints):
             self.neutral_trs[i] = TRS(joint)
 
-        self.meshes = pm.listRelatives(self.root_joint, allDescendents=True, type="mesh")
+        self.meshes = [x.getParent() for x in pm.listRelatives(self.root_joint, allDescendents=True, type="mesh")]
 
         ui.set_value(self.text_root, self.root_joint)
+
+    def onSetJoints(self, *args):
+        selection = pm.selected(flatten=True, type="joint")
+        if selection:
+            self.joints = selection
+            self.root_joint = self.joints[0]
+
+        self.neutral_trs = [None] * len(self.joints)
+
+        for i, joint in enumerate(self.joints):
+            self.neutral_trs[i] = TRS(joint)
+
+        ui.set_value(self.text_root, self.root_joint)
+
+    def onSetMeshes(self, *args):
+        selection = pm.selected(flatten=True)
+        if selection:
+            self.meshes = selection
+
+        ui.set_value(self.text_meshes, str(len(self.meshes)))
 
     def onDragTranslateX(self, *args):
         self.onChangeTranslateX(*args)
@@ -187,93 +255,122 @@ class NN_ToolWindow(object):
         if ui.is_shift():
             ui.set_value(self.fs_tra_x, value=0)
 
-        active_obj = self.joints[self.cursor]
         neutral = self.neutral_trs[self.cursor].translateX
         v = ui.get_value(self.fs_tra_x)
-        active_obj.translateX.set(neutral + v * self.translate_factor())
+        self.current_joint().translateX.set(neutral + v * self.translate_factor())
 
     def onChangeTranslateY(self, *args):
         if ui.is_shift():
             ui.set_value(self.fs_tra_y, value=0)
 
-        active_obj = self.joints[self.cursor]
         neutral = self.neutral_trs[self.cursor].translateY
         v = ui.get_value(self.fs_tra_y)
-        active_obj.translateY.set(neutral + v * self.translate_factor())
+        self.current_joint().translateY.set(neutral + v * self.translate_factor())
 
     def onChangeTranslateZ(self, *args):
         if ui.is_shift():
             ui.set_value(self.fs_tra_z, value=0)
 
-        active_obj = self.joints[self.cursor]
         neutral = self.neutral_trs[self.cursor].translateZ
         v = ui.get_value(self.fs_tra_z)
-        active_obj.translateZ.set(neutral + v * self.translate_factor())
+        self.current_joint().translateZ.set(neutral + v * self.translate_factor())
 
     def onChangeRotateX(self, *args):
         if ui.is_shift():
             ui.set_value(self.fs_rot_x, value=0)
 
-        active_obj = self.joints[self.cursor]
         neutral = self.neutral_trs[self.cursor].rotateX
         v = ui.get_value(self.fs_rot_x)
-        active_obj.rotateX.set(neutral + v * self.rotate_factor())
+        self.current_joint().rotateX.set(neutral + v * self.rotate_factor())
 
     def onChangeRotateY(self, *args):
         if ui.is_shift():
             ui.set_value(self.fs_rot_y, value=0)
 
-        active_obj = self.joints[self.cursor]
         neutral = self.neutral_trs[self.cursor].rotateY
         v = ui.get_value(self.fs_rot_y)
-        active_obj.rotateY.set(neutral + v * self.rotate_factor())
+        self.current_joint().rotateY.set(neutral + v * self.rotate_factor())
 
     def onChangeRotateZ(self, *args):
         if ui.is_shift():
             ui.set_value(self.fs_rot_z, value=0)
 
-        active_obj = self.joints[self.cursor]
         neutral = self.neutral_trs[self.cursor].rotateZ
         v = ui.get_value(self.fs_rot_z)
-        active_obj.rotateZ.set(neutral + v * self.rotate_factor())
+        self.current_joint().rotateZ.set(neutral + v * self.rotate_factor())
 
     def onGradation(self, *args):
-        nm.weight_paint_mode_with_selected_joint(joint=self.current_joint(), meshes=self.meshes)
+        if is_weight_paint_mode():
+            activate_select_mode()
+            pm.select(self.current_joint())
+        
+        else:
+            weight_paint_mode_with_selected_joint(joint=self.current_joint(), meshes=self.meshes)
 
     def onAnimation(self, *args):
         pass
 
-    def onPrev(self, *args):
+    def onPrevFocus(self, *args):
+        self.onPrevNoFocus()
+        focus_object(self.current_joint())
+
+    def onResetFocus(self, *args):
+        self.onResetNoFocus()
+        focus_object(self.current_joint())
+
+    def onNextFocus(self, *args):
+        self.onNextNoFocus()
+        focus_object(self.current_joint())
+
+    def onPrevNoFocus(self, *args):
+        paint_mode = is_weight_paint_mode()
         self.cursor -= 1
 
         if self.cursor < 0:
             nd.message("finish")
             self.cursor = len(self.joints) - 1
 
-        active_obj = self.joints[self.cursor]
-        pm.select(active_obj)
-        pm.viewFit(animate=True, fitFactor=self.fit_factor)
-        ui.set_value(self.text_current, active_obj)
+        ui.set_value(self.text_current, self.current_joint())
 
-    def onReset(self, *args):
+        if paint_mode:
+            change_paint_target_influence(joint=self.current_joint())
+        
+        else:
+            pm.select(self.current_joint())
+
+    def onResetNoFocus(self, *args):
+        paint_mode = is_weight_paint_mode()
         self.cursor = 0
 
-        active_obj = self.joints[self.cursor]
-        pm.select(active_obj)
-        pm.viewFit(animate=True, fitFactor=self.fit_factor)
-        ui.set_value(self.text_current, active_obj)
+        ui.set_value(self.text_current, self.current_joint())
 
-    def onNext(self, *args):
+        if paint_mode:
+            change_paint_target_influence(joint=self.current_joint())
+
+        else:
+            pm.select(self.current_joint())
+
+    def onNextNoFocus(self, *args):
+        paint_mode = is_weight_paint_mode()
+        print(paint_mode)
         self.cursor += 1
 
         if self.cursor >= len(self.joints):
             nd.message("finish")
             self.cursor = 0
 
-        active_obj = self.joints[self.cursor]
-        pm.select(active_obj)
-        pm.viewFit(animate=True, fitFactor=self.fit_factor)
-        ui.set_value(self.text_current, active_obj)
+        ui.set_value(self.text_current, self.current_joint())
+
+        if paint_mode:
+            change_paint_target_influence(joint=self.current_joint())
+
+        else:
+            pm.select(self.current_joint())
+
+    def onPaintMode(self, *args):
+        import nnutil.misc as nm
+
+        weight_paint_mode_with_selected_joint()
 
 
 def showNNToolWindow():
