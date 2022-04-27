455509e02b152fec00add93b481511137023b3c2
ichiyonnana <ichiyonnana@gmail.com>
2022-04-28 03:28:17 +0900

    ダイアログのテンプレート修正 最大化最小化ボタン削除 ウィンドウサイズ固定 毎回指定したサイズで起動するように変更

--- _template/core.py
+++ _template/core.py
@@ -1,10 +1,8 @@
 #! python
 # coding:utf-8
-
 """
-
+ツールの概要
 """
-import maya.cmds as cmds
 import pymel.core as pm
 
 import nnutil.core as nu
@@ -12,41 +10,54 @@ import nnutil.decorator as deco
 import nnutil.ui as ui
 import nnutil.display as nd
 
-dialog_name = "NN_"
+window_name = "NN_Tools"
 
 
 class NN_ToolWindow(object):
     def __init__(self):
-        self.window = dialog_name
-        self.title = dialog_name
-        self.size = (300, 95)
+        self.window = window_name
+        self.title = window_name
+        self.size = (251, 220)
+
+        self.is_chunk_open = False
 
     def create(self):
-        if cmds.window(self.window, exists=True):
-            cmds.deleteUI(self.window, window=True)
-        self.window = cmds.window(
-            self.window,
-            t=self.title,
-            widthHeight=self.size
-        )
+        if pm.window(self.window, exists=True):
+            pm.deleteUI(self.window, window=True)
+
+        # プリファレンスの有無による分岐
+        if pm.windowPref(self.window, exists=True):
+            # ウィンドウのプリファレンスがあれば位置だけ保存して削除
+            position = pm.windowPref(self.window, q=True, topLeftCorner=True)
+            pm.windowPref(self.window, remove=True)
+
+            # 前回位置に指定したサイズで表示
+            pm.window(self.window, t=self.title, maximizeButton=False, minimizeButton=False, topLeftCorner=position, widthHeight=self.size, sizeable=False)
+
+        else:
+            # プリファレンスがなければデフォルト位置に指定サイズで表示
+            pm.window(self.window, t=self.title, maximizeButton=False, minimizeButton=False, widthHeight=self.size, sizeable=False)
+
         self.layout()
-        cmds.showWindow()
+        pm.showWindow(self.window)
 
     def layout(self):
         ui.column_layout()
 
         ui.row_layout()
+        ui.header(label="sample:")
+        ui.button(label="Test", c=self.onTest)
         ui.end_layout()
 
         ui.end_layout()
 
-
-def showNNToolWindow():
-    NN_ToolWindow().create()
+    def onTest(self, *args):
+        """Testハンドラ"""
+        pass
 
 
 def main():
-    showNNToolWindow()
+    NN_ToolWindow().create()
 
 
 if __name__ == "__main__":
