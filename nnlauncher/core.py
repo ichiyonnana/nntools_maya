527955a37e7a24a32d110070cb472b5569db8711
ichiyonnana <ichiyonnana@gmail.com>
2022-06-07 03:54:06 +0900

    ランチャー作成

--- nnlauncher/core.py
+++ nnlauncher/core.py
@@ -1,103 +1,257 @@
 #! python
 # coding:utf-8
+"""
+ツールの概要
+"""
+from email import header
+import pymel.core as pm
 
-# ダイアログのテンプレ
-# self.window だけユニークならあとはそのままで良い
-import re
-import os
-import sys
-import traceback
+import nnutil.core as nu
+import nnutil.decorator as deco
+import nnutil.ui as ui
+import nnutil.display as nd
 
-import maya.cmds as cmds
-import maya.mel as mel
+from PySide2 import QtGui
 
-import nnutil
+window_name = "NN_Launcher"
+window = None
 
-window_width = 600
-header_width = 50
-color_x = (1.0, 0.5, 0.5)
-color_y = (0.5, 1.0, 0.5)
-color_z = (0.5, 0.5, 1.0)
-color_joint = (0.5, 1.0, 0.75)
-color_select = (0.5, 0.75, 1.0)
-bw_single = 24
-bw_double = bw_single*2 + 2
+
+def get_window():
+    return window
+
 
 class NN_ToolWindow(object):
+    all_modules = [
+        "nnmirror.core",
+        "nnuvtoolkit.core",
+        "nncamera.core",
+        "align_edgering_length.core",
+        "nncurve.core",
+        "nnsimplify.core",
+        "nnstraighten.core",
+        "nnlattice.core",
+        "altunt.core",
+        "nnvcolor.core",
+        "nnskin.core",
+        "nnsubdiv.core",
+        "nntransform.core",
+        ]
 
     def __init__(self):
-        self.window = 'NN_Launcher'
-        self.title = 'NN_Launcher'
-        self.size = (window_width, 95)
+        self.window = window_name
+        self.title = window_name
+        self.size = (280, 260)
+
+        self.common_button_height = 1.5
+
+        self.is_chunk_open = False
 
     def create(self):
-        if cmds.window(self.window, exists=True):
-            cmds.deleteUI(self.window, window=True)
-        self.window = cmds.window(
-            self.window,
-            t=self.title,
-            widthHeight=self.size
-        )
+        if pm.window(self.window, exists=True):
+            pm.deleteUI(self.window, window=True)
+
+        cursor_pos = QtGui.QCursor().pos()
+
+        # プリファレンスの有無による分岐
+        if pm.windowPref(self.window, exists=True):
+            # ウィンドウのプリファレンスがあれば位置だけ保存して削除
+            position = (cursor_pos.y(), cursor_pos.x())
+            pm.windowPref(self.window, remove=True)
+
+            # 前回位置に指定したサイズで表示
+            pm.window(self.window, t=self.title, maximizeButton=False, minimizeButton=False, topLeftCorner=position, widthHeight=self.size, sizeable=False)
+
+        else:
+            # プリファレンスがなければデフォルト位置に指定サイズで表示
+            pm.window(self.window, t=self.title, maximizeButton=False, minimizeButton=False, widthHeight=self.size, sizeable=False)
+
         self.layout()
-        cmds.showWindow()
+        pm.showWindow(self.window)
 
     def layout(self):
-        self.columnLayout = cmds.columnLayout()
+        ui.column_layout()
 
-        self.rowLayout1 = cmds.rowLayout( numberOfColumns=16 )
-        self.label1 = cmds.text( label='NNTools' ,width=header_width)
-        self.bt_ = cmds.button(l='Mirror', c=self.onMirror, width=bw_double)
-        self.bt_ = cmds.button(l='Curve', c=self.onCurve, width=bw_double)
-        self.bt_ = cmds.button(l='EdgeRing', c=self.onEdgeRing, width=bw_double)
-        self.bt_ = cmds.button(l='Skin', c=self.onSkin, width=bw_double)
-        self.bt_ = cmds.button(l='Transform', c=self.onTransform, width=bw_double)
-        self.bt_ = cmds.button(l='UV', c=self.onUV, width=bw_double)
-        self.bt_ = cmds.button(l='Subdiv', c=self.onSubdiv, width=bw_double)
-        self.bt_ = cmds.button(l='Manip', c=self.onManip, width=bw_double)
-        cmds.setParent("..")
+        ui.row_layout()
+        ui.header(label="Editor:")
+        ui.button(label="Mirror", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onMirror)
+        ui.button(label="UV", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onUV)
+        ui.button(label="Camera", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onCamera)
+        ui.end_layout()
 
-        cmds.separator(width=window_width)
+        ui.row_layout()
+        ui.header(label="Align:")
+        ui.button(label="EdgeRing", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onEdgeRing)
+        ui.button(label="Curve", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onCurve)
+        ui.button(label="Simplify", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onSimplify)
+        ui.button(label="Straighten", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onStraighten)
+        ui.end_layout()
+
+        ui.row_layout()
+        ui.header(label="Util:")
+        ui.button(label="Lattice", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onLattice)
+        ui.button(label="Normal", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onNormal)
+        ui.button(label="VColor", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onVColor)
+        ui.end_layout()
+
+        ui.row_layout()
+        ui.header(label="Etc")
+        ui.button(label="SKin", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onSKin)
+        ui.button(label="Subdiv", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onSubdiv)
+        ui.button(label="Transform", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onTransform)
+        ui.end_layout()
+
+        ui.row_layout()
+        ui.header(label="")
+        ui.button(label="Close All", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onCloseAll)
+        ui.button(label="Reload All", width=ui.width(2), height=ui.height(self.common_button_height), c=self.onReloadAll)
+        ui.end_layout()
+
+        ui.separator(height=ui.height(0.5))
+
+        ui.row_layout()
+        ui.header(label="")
+        ui.button(label="Close", width=ui.width(8), height=ui.height(self.common_button_height), c=self.onClose)
+        ui.end_layout()
+
+        ui.end_layout()
 
     def onMirror(self, *args):
-        import nnmirror
-        nnmirror.main()
-
-    def onCurve(self, *args):
-        import align_edges_on_curve
-        align_edges_on_curve.main()
-
-    def onEdgeRing(self, *args):
-        import align_edgering_length
-        align_edgering_length.main()
-
-    def onSkin(self, *args):
-        import nnskin
-        nnskin.main()
-
-    def onSubdiv(self, *args):
-        import nnsubdiv
-        nnsubdiv.main()
-
-    def onTransform(self, *args):
-        import nntransform
-        nntransform.main()
+        """"""
+        import nnmirror.core
+        nnmirror.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
 
     def onUV(self, *args):
-        import nnuvtoolkit
-        nnuvtoolkit.main()
+        """"""
+        import nnuvtoolkit.core
+        nnuvtoolkit.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
 
-    def onManip(self, *args):
-        import nnmanip
-        nnmanip.main()
+    def onCamera(self, *args):
+        """"""
+        import nncamera.core
+        nncamera.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onEdgeRing(self, *args):
+        """"""
+        import align_edgering_length.core
+        align_edgering_length.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onCurve(self, *args):
+        """"""
+        import nncurve.core
+        nncurve.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onSimplify(self, *args):
+        """"""
+        import nnsimplify.core
+        nnsimplify.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onStraighten(self, *args):
+        """"""
+        import nnstraighten.core
+        nnstraighten.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onLattice(self, *args):
+        """"""
+        import nnlattice.core
+        nnlattice.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onNormal(self, *args):
+        """"""
+        import altunt.core
+        altunt.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onVColor(self, *args):
+        """"""
+        import nnvcolor.core
+        nnvcolor.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onSKin(self, *args):
+        """"""
+        import nnskin.core
+        nnskin.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onSubdiv(self, *args):
+        """"""
+        import nnsubdiv.core
+        nnsubdiv.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onTransform(self, *args):
+        """"""
+        import nntransform.core
+        nntransform.core.main()
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onCloseAll(self, *args):
+        """全ての NNTools ダイアログを閉じる"""
+        for module_name in self.all_modules:
+            module = __import__(module_name)
+            if pm.window(module.window_name, exists=True):
+                pm.deleteUI(module.window_name, window=True)
+
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onReloadAll(self, *args):
+        """全ての NNTools のモジュールをリロードする"""
+        for module_name in self.all_modules:
+            print("reload %s" % module_name)
+            module = __import__(module_name)
+            reload(module.core)
+
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
+
+    def onClose(self, *args):
+        """ランチャーを閉じる"""
+        if not ui.is_shift():
+            pm.deleteUI(self.window, window=True)
 
 
+def smart_launch():
+    """選択オブジェクト等から自動で適切なツールを起動する"""
+    print("smart_launch")
 
-
-def showNNToolWindow():
-    NN_ToolWindow().create()
 
 def main():
-    showNNToolWindow()
+    """ウインドウの表示
+    
+    すでに表示されていた場合は閉じるだけ
+    """
+    global window
+    window = NN_ToolWindow()
+
+    if pm.window(window.window, exists=True):
+        pm.deleteUI(window.window, window=True)
+
+    else:
+        window.create()
+
 
 if __name__ == "__main__":
     main()
