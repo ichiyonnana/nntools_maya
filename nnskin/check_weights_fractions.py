ecc86ed7c8e64f40cdde4eaf9f9a1fc7383ea4ee
ichiyonnana <ichiyonnana@gmail.com>
2022-06-07 04:02:07 +0900

    ウェイトの端数チェックを関数化

--- nnskin/check_weights_fractions.py
+++ nnskin/check_weights_fractions.py
@@ -1,68 +1,81 @@
+#! python
+# coding:utf-8
+
 import maya.cmds as cmds
 import maya.mel as mel
+
 
 # 修正処理
 def fix_command():
     print("dummy_fix_command")
 
-# 設定
-min_unit = 0.01
-model_root_grp = "Root"
 
-selections = cmds.ls(selection=True, flatten=True)
-objects = None
+def main(model_root_grp=None, min_unit=0.01, fast_mode=True):
 
-if len(selections) != 0:
-    objects = selections
-else:
-    objects = cmds.listRelatives(model_root_grp)
+    selections = cmds.ls(selection=True, flatten=True)
+    objects = None
 
-fraction_objs = []
-
-for obj in objects:
-    sc = mel.eval('findRelatedSkinCluster %(obj)s' % locals() )
-    if sc == "":
-        next
+    if len(selections) != 0:
+        objects = selections
     else:
-        cmds.select(obj)
-        cmds.ConvertSelectionToVertices()
-        vertices = cmds.ls(selection=True, flatten=True)
+        if model_root_grp:
+            objects = cmds.listRelatives(model_root_grp)
+        else:
+            print("no target")
 
-        has_fraction = False
+    fraction_objs = []
+    fraction_comp = []
 
-        for vtx in vertices:
-            weights = cmds.skinPercent(sc, vtx, q=True, v=True)
+    for obj in objects:
+        sc = mel.eval('findRelatedSkinCluster %(obj)s' % locals())
+        if sc == "":
+            next
+        else:
+            print("check %s" % obj)
 
-            for w in weights:
-                f1 = (w % min_unit)
-                f2 = min_unit - f1
-                f = min(f1, f2)
-                t = 0.00000000000001 # 無視する浮動小数誤差
-                fraction_is_too_small = (f < t)
+            cmds.select(obj)
+            cmds.ConvertSelectionToVertices()
+            vertices = cmds.ls(selection=True, flatten=True)
 
-                if not fraction_is_too_small:
-                    has_fraction = True
-                    print(vtx)
+            has_fraction = False
+
+            for vtx in vertices:
+                weights = cmds.skinPercent(sc, vtx, q=True, v=True)
+
+                for w in weights:
+                    f1 = (w % min_unit)
+                    f2 = min_unit - f1
+                    f = min(f1, f2)
+                    t = 0.00000000000001  # 無視する浮動小数誤差
+                    fraction_is_too_small = (f < t)
+
+                    if not fraction_is_too_small:
+                        has_fraction = True
+                        fraction_comp.append(vtx)
+
+                if fast_mode and has_fraction:
+                    break
 
             if has_fraction:
-                break
+                fraction_objs.append(obj)
 
-        if has_fraction:
-            fraction_objs.append(obj)
+    message = ""
 
-message = ""
+    if len(fraction_objs) == 0:
+        message = "OK"
+        cmds.select(clear=True)
+        cmds.SelectToggleMode()
+        cmds.confirmDialog(title='has weight fractions', message=message)
 
-if len(fraction_objs) == 0:
-    message = "OK"
-    cmds.select(clear=True)
-    cmds.SelectToggleMode()
-    cmds.confirmDialog( title='has weight fractions', message=message)
+    else:
+        cmds.SelectToggleMode()
+        cmds.select(fraction_comp)
+        message = u"ウェイトに%(min_unit)s以下の値が使われています\n\n" % locals() + ", ".join(fraction_objs)
+        ret = cmds.confirmDialog(title=u"ウェイト少数チェック", message=message, button=["Fix","Cancel"], defaultButton="Fix", cancelButton="Cancel", dismissString="Cancel")
 
-else:
-    cmds.SelectToggleMode()
-    cmds.select(fraction_objs)
-    message = u"ウェイトに%(min_unit)s以下の値が使われています\n\n" % locals() + ", ".join(fraction_objs)
-    ret = cmds.confirmDialog(title=u"ウェイト少数チェック", message=message, button=["Fix","Cancel"], defaultButton="Fix", cancelButton="Cancel", dismissString="Cancel")
+        if (ret == "Fix"):
+            fix_command()
 
-    if (ret == "Fix"):
-        fix_command()
+
+if __name__ == "__main__":
+    main()
