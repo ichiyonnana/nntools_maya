f6afd707548ae2846576e990489a76236bf4270f
ichiyonnana <ichiyonnana@gmail.com>
2022-07-04 10:34:36 +0900

    シミュレーションがワールド空間で計算されるように修正

--- nnanim/core.py
+++ nnanim/core.py
@@ -3,7 +3,9 @@
 """
 リグ･アニメーション関連
 """
+import re
 import pymel.core as pm
+import pymel.core.nodetypes as nt
 import maya.mel as mel
 
 import nnutil.core as nu
@@ -35,13 +37,20 @@ class NN_ToolWindow(object):
         self.hair_system_grp_name = "NNANIMHS_hairSystemGrp"
         self.nucleus_name = "NNANIMHS_nucleus"
         self.hair_curves_grp_name = "NNANIMHS_hairSystemGrpOutputCurves"
-        self.hair_curve_name = "NNANIMHS_curve"
+        self.hair_curve_name = "NNANIMHS_dynamicscurve"
+        self.base_curve_name = "NNANIMHS_basecurve"
         self.hair_follicle_grp_name = "NNANIMHS_hairSystemGrpFollicles"
         self.follicle_name = "NNANIMHS_follicle"
         self.hair_ik_grp_name = "NNANIMHS_objects"
 
         self.hair_system = None
         self.nucleus = None
+
+        if pm.objExists(self.hair_system_name):
+            self.hair_system = pm.PyNode(self.hair_system_name)
+
+        if pm.objExists(self.nucleus_name):
+            self.nucleus = pm.PyNode(self.nucleus_name)
 
     def create(self):
         if pm.window(self.window, exists=True):
@@ -200,42 +209,70 @@ class NN_ToolWindow(object):
 
         self.hair_system.active.set(1)
 
+    def get_duplicated_joint_name(self, name):
+        base_name = re.sub(r".*\|", "", name)
+        return self.dup_prefix + base_name
+
     def onMakeIKHandleHair(self, *args):
         """nHair をハンドルとしたスプラインIKを作成する
         """
+        self.dup_prefix = "dup_"
+
+        selection = pm.selected()
+
+        if len(selection) < 2:
+            print("select 2 joints ")
+            return
+
         start_joint, end_joint = pm.selected()[0:2]
 
-        # ジョイントからカーブを作成する
+        # IK 開始ジョイントの親から複製してリネーム
+        if start_joint.getParent():
+            # start_joint に親がある場合
+            parent_joint = start_joint.getParent()
+            dup_parent_joint = pm.duplicate(parent_joint)[0]
+            pm.rename(dup_parent_joint, self.get_duplicated_joint_name(parent_joint.fullPathName()))
+            dup_tree = pm.listRelatives(dup_parent_joint, allDescendents=True)
+
+        else:
+            # start_joint に親がない場合
+            dup_parent_joint = pm.createNode("joint")
+            dup_start_joint = pm.duplicate(start_joint)
+            pm.parent(dup_start_joint, dup_parent_joint)
+            pm.rename(dup_parent_joint, "dup_root")
+            dup_tree = pm.listRelatives(dup_parent_joint, allDescendents=True)
+
+        for obj in dup_tree:
+            pm.rename(obj, self.get_duplicated_joint_name(obj.fullPathName()))
+
+        # 複製ジョイントを非表示にする
+        dup_parent_joint.visibility.set(False)
+
+        # 複製ジョイントに splineIK を作成
+        dup_start_joint = pm.PyNode(self.get_duplicated_joint_name(start_joint.name()))
+        dup_end_joint = pm.PyNode(self.get_duplicated_joint_name(end_joint.name()))
         span = 10
         # pm.curve(d=2, p=[(),(),()])
-        handle, effector, curve = pm.ikHandle(sol="ikSplineSolver", startJoint=start_joint, endEffector=end_joint, name=self.handle_name, numSpans=span)
-        pm.delete(handle)
-        pm.parent(curve, world=True)
+        handle, effector, base_curve = pm.ikHandle(sol="ikSplineSolver", startJoint=dup_start_joint, endEffector=dup_end_joint, name=self.handle_name, numSpans=span)
+        pm.rename(base_curve, self.base_curve_name)
 
         # カーブをnHairダイナミクスに変換
         self._create_hair_system()
-        pm.select([curve, self.hair_system], self.hair_system, replace=True)
+        pm.select([base_curve, self.hair_system], self.hair_system, replace=True)
         mel.eval('makeCurvesDynamic 2 { "0", "0", "1", "1", "0"};')
-        curve.visibility.set(False)
+        base_curve.visibility.set(False)
         hair_curve = pm.PyNode(self.hair_curves_grp_name).getChildren()[-1]
         hair_curve.rename(self.hair_curve_name)
 
         # follicle の固定を根元だけに変更
-        follicle = pm.PyNode(self.hair_follicle_grp_name).getChildren()[-1]
-        follicle.pointLock.set(1)
+        follicle = base_curve.getParent()
+        follicle_shape = follicle.getShape()
+        follicle_shape.pointLock.set(1)
         follicle.rename(self.follicle_name)
 
-        # カーブを使用してスプラインIK作成
-        handle, effector = pm.ikHandle(sol="ikSplineSolver", startJoint=start_joint, endEffector=end_joint, name=self.handle_name, numSpans=span, createCurve=False, curve=hair_curve)
+        # シミュレーションされるカーブを IKHandle に接続
+        hair_curve.getShape().worldSpace.connect(handle.inCurve, force=True)
         handle.visibility.set(False)
-
-        self.ik_handle = handle
-        self.polevector_locator = None
-        self.spline_curve = hair_curve
-
-        # ダイナミクス用カーブはスタートジョイントと兄弟にする
-        if start_joint.getParent():
-            hair_curve.setParent(start_joint.getParent())
 
         # グループにまとめる
         if not pm.objExists(self.hair_ik_grp_name):
@@ -245,10 +282,34 @@ class NN_ToolWindow(object):
         handle.setParent(hair_ik_grp)
         pm.PyNode(self.hair_system_grp_name).setParent(hair_ik_grp)
         pm.PyNode(self.nucleus_name).setParent(hair_ik_grp)
-        pm.PyNode(self.hair_follicle_grp_name).setParent(hair_ik_grp)
         pm.PyNode(self.hair_curves_grp_name).setParent(hair_ik_grp)
+        dup_parent_joint.setParent(hair_ik_grp)
+
+        # オリジナルジョイントから複製ジョイントへペアレントコンストレイン作成
+        pm.parentConstraint(parent_joint, dup_parent_joint, maintainOffset=True)
+
+        # 複製ジョイントから rotate を接続
+        start_depth = start_joint.fullPathName().count("|")
+        end_depth = end_joint.fullPathName().count("|")
+
+        for i in range(start_depth, end_depth+1):
+            orig_joint_name = re.search(r"^(?:\|[^|]+){%s}" % i, end_joint.fullPathName()).group()
+            dup_joint_name = self.get_duplicated_joint_name(orig_joint_name)
+            pm.PyNode(dup_joint_name).rotate.connect(pm.PyNode(orig_joint_name).rotate)
+
+        # follicle 関連のアトリビュート接続
+        dup_start_joint.worldMatrix.connect(follicle_shape.inputWorldMatrix)
+        base_curve_shape = base_curve.getShape()
+        base_curve_shape.local.connect(follicle_shape.startPosition, force=True)
+        base_curve.worldMatrix.connect(follicle_shape.startPositionMatrix, force=True)
+        follicle_shape.outRotate.connect(follicle.rotate)
+        follicle_shape.outTranslate.connect(follicle.translate)
 
         # UI の更新
+        self.ik_handle = handle
+        self.polevector_locator = None
+        self.spline_curve = hair_curve
+
         n = self.spline_curve.numCVs() - 1
         pm.intSlider(self.sl_cv_index, e=True, max=n)
 
