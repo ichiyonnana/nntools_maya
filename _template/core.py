f02db852b4b1d1ec5626faf1a4fc385ae6fcab35
ichiyonnana <ichiyonnana@gmail.com>
2023-04-10 07:10:11 +0900

    テンプレートの修正
    他プロジェクトの最新版の内容を反映
    UIスケールやフォントサイズでUIが崩れないように resizeToFitChildren 追加

--- _template/core.py
+++ _template/core.py
@@ -1,8 +1,7 @@
 #! python
 # coding:utf-8
-"""
-ツールの概要
-"""
+"""ツールの概要."""
+
 import pymel.core as pm
 
 import nnutil.core as nu
@@ -14,14 +13,28 @@ window_name = "NN_Tools"
 
 
 class NN_ToolWindow(object):
+    """ここにツールの説明."""
+
+    window_width = 251
+    window_height = 220
+
     def __init__(self):
+        """コンストラクタ.
+
+        UIウィンドウの設定
+        コンポーネント選択順序を保存するプリファレンスの設定
+        Undo チャンク用のフラグ初期化
+        """
         self.window = window_name
         self.title = window_name
-        self.size = (251, 220)
+        self.size = (self.window_width, self.window_height)
+
+        pm.selectPref(trackSelectionOrder=True)
 
         self.is_chunk_open = False
 
     def create(self):
+        """ウィンドウの作成."""
         if pm.window(self.window, exists=True):
             pm.deleteUI(self.window, window=True)
 
@@ -32,16 +45,34 @@ class NN_ToolWindow(object):
             pm.windowPref(self.window, remove=True)
 
             # 前回位置に指定したサイズで表示
-            pm.window(self.window, t=self.title, maximizeButton=False, minimizeButton=False, topLeftCorner=position, widthHeight=self.size, sizeable=False)
+            self.window = pm.window(
+                self.window,
+                t=self.title,
+                widthHeight=self.size,
+                sizeable=False,
+                maximizeButton=False,
+                minimizeButton=False,
+                resizeToFitChildren=True,
+                topLeftCorner=position
+            )
 
         else:
             # プリファレンスがなければデフォルト位置に指定サイズで表示
-            pm.window(self.window, t=self.title, maximizeButton=False, minimizeButton=False, widthHeight=self.size, sizeable=False)
+            self.window = pm.window(
+                self.window,
+                t=self.title,
+                widthHeight=self.size,
+                sizeable=False,
+                maximizeButton=False,
+                minimizeButton=False,
+                resizeToFitChildren=True
+            )
 
         self.layout()
         pm.showWindow(self.window)
 
     def layout(self):
+        """UI レイアウト."""
         ui.column_layout()
 
         ui.row_layout()
@@ -52,11 +83,12 @@ class NN_ToolWindow(object):
         ui.end_layout()
 
     def onTest(self, *args):
-        """Testハンドラ"""
+        """Testハンドラ."""
         pass
 
 
 def main():
+    """メイン関数."""
     NN_ToolWindow().create()
 
 
