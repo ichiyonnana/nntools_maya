2bc0ef2a00d42ac5f9278a9484684caf14e83ceb
ichiyonnana <ichiyonnana@gmail.com>
2024-09-16 21:52:06 +0900

    nntransform

--- nntransform/core.py
+++ nntransform/core.py
@@ -6,7 +6,6 @@ import os
 import sys
 import traceback
 
-import pymel.core as pm
 import maya.cmds as cmds
 import maya.mel as mel
 
@@ -40,17 +39,17 @@ class NN_ToolWindow(object):
         self.size = (window_width, 210)
 
     def create(self):
-        if pm.window(self.window, exists=True):
-            pm.deleteUI(self.window, window=True)
+        if cmds.window(self.window, exists=True):
+            cmds.deleteUI(self.window, window=True)
 
         # プリファレンスの有無による分岐
-        if pm.windowPref(self.window, exists=True):
+        if cmds.windowPref(self.window, exists=True):
             # ウィンドウのプリファレンスがあれば位置だけ保存して削除
-            position = pm.windowPref(self.window, q=True, topLeftCorner=True)
-            pm.windowPref(self.window, remove=True)
+            position = cmds.windowPref(self.window, q=True, topLeftCorner=True)
+            cmds.windowPref(self.window, remove=True)
 
             # 前回位置に指定したサイズで表示
-            pm.window(
+            cmds.window(
                 self.window,
                 t=self.title,
                 maximizeButton=False,
@@ -63,7 +62,7 @@ class NN_ToolWindow(object):
 
         else:
             # プリファレンスがなければデフォルト位置に指定サイズで表示
-            pm.window(
+            cmds.window(
                 self.window,
                 t=self.title,
                 maximizeButton=False,
@@ -74,7 +73,7 @@ class NN_ToolWindow(object):
                 )
 
         self.layout()
-        pm.showWindow(self.window)
+        cmds.showWindow(self.window)
 
     def layout(self):
         self.columnLayout = cmds.columnLayout()
