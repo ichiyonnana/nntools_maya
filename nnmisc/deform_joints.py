8d70db0f9ccf59ef4fcf9274ab36560d0e4becc5
ichiyonnana <ichiyonnana@gmail.com>
2024-09-16 20:32:10 +0900

    deform_joints

--- nnmisc/deform_joints.py
+++ nnmisc/deform_joints.py
@@ -2,27 +2,27 @@
 ジョイントと頂点が 1:1 になるポリゴンを作成する
 ポリゴンをデフォームした後ジョイントを対応する頂点に移動する
 """
+import maya.cmds as cmds
 
-import pymel.core as pm
+import nnutil.core as nu
 
 proxy_obj_name = "jlproxyobj"
 
 
 def deform_joints_prepare():
     """ジョイントと頂点が 1:1 になるポリゴンを作成する"""
-    all_joints = pm.ls(type="joint")
-    positions = [x.getTranslation(space="world") for x in all_joints]
+    all_joints = cmds.ls(type="joint")
+    positions = [cmds.xform(x, q=True, translation=True, worldSpace=True) for x in all_joints]
 
-    obj_name, shape = pm.polyCreateFacet(ch=True, tx=1, s=1, p=positions)
-    obj = pm.PyNode(obj_name)
+    obj_name, shape = cmds.polyCreateFacet(ch=True, tx=1, s=1, p=positions)
 
-    obj.overrideEnabled.set(1)
-    obj.overrideEnabled.set(1)
-    obj.overrideShading.set(0)
-    obj.overrideTexturing.set(0)
-    obj.overridePlayback.set(0)
+    cmds.setAttr(obj_name + ".overrideEnabled", 1)
+    cmds.setAttr(obj_name + ".overrideEnabled", 1)
+    cmds.setAttr(obj_name + ".overrideShading", 0)
+    cmds.setAttr(obj_name + ".overrideTexturing", 0)
+    cmds.setAttr(obj_name + ".overridePlayback", 0)
 
-    obj.rename(proxy_obj_name)
+    cmds.rename(obj_name, proxy_obj_name)
 
 
 def hierarchy_depth(obj_name):
@@ -34,24 +34,21 @@ def hierarchy_depth(obj_name):
     Returns:
         int: DAG の深さを表す整数
     """
-    obj = pm.PyNode(obj_name)
-
-    return obj.longName().count("|")
+    return cmds.ls(obj_name, long=True).count("|")
 
 
 def deform_joints_apply():
     """デフォームされたポリゴンに一致するようにジョイントを対応する頂点に移動する"""
-    obj = pm.PyNode(proxy_obj_name)
-    positions = obj.getShape().getPoints(space="world")
+    positions = nu.get_points(proxy_obj_name, space="world")
 
-    all_joints = pm.ls(type="joint")
+    all_joints = cmds.ls(type="joint")
 
     joint_pos_table = {}
 
     for i in range(len(all_joints)):
-        joint_pos_table[all_joints[i].name()] = positions[i]
+        joint_pos_table[all_joints[i]] = positions[i]
 
-    all_joints.sort(key=lambda x: hierarchy_depth(x.name()))
+    all_joints.sort(key=lambda x: hierarchy_depth(x))
 
     for joint in all_joints:
-        joint.setTranslation(joint_pos_table[joint.name()], space="world")
+        cmds.xform(joint, translation=list(joint_pos_table[joint]), worldSpace=True)
